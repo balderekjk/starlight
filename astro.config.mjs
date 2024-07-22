eeda37dad46f3832351a1438d9227ff9adefe00c
Derek Bal <bal.derek.j@gmail.com>
2024-07-21 19:53:24 -1000

    checkpoint

---
+++ astro.config.mjs
@@ -0,0 +1,35 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Placeholder",
+      components: {
+        Header: "./src/components/Header.astro",
+      },
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+      customCss: ["./src/tailwind.css"],
+      tableOfContents: false,
+      pagination: false,
+    }),
+    tailwind({ applyBaseStyles: false }),
+  ],
+});
