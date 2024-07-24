04b0b2aa1f0353cdca00cad36e7029e4354879d6
Derek Bal <bal.derek.j@gmail.com>
2024-07-23 23:42:12 -1000

    checkpt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,28 +2,33 @@ import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 
+export const MAIN_NAV = [
+  { label: "home", slug: "home" },
+  { label: "annual convention", slug: "annual-convention" },
+  { label: "register", slug: "register" },
+  { label: "about us", slug: "about-us" },
+];
+
 // https://astro.build/config
 export default defineConfig({
+  redirects: { "/": "/home" },
   integrations: [
     starlight({
       title: "NHEA",
       sidebar: [
         {
           label: "Navigation",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "home", slug: "" },
-            { label: "annual convention", slug: "annual-convention" },
-            { label: "register", slug: "register" },
-            { label: "about us", slug: "about-us" },
-          ],
+          items: MAIN_NAV,
         },
         {
           label: "Reference",
-          autogenerate: { directory: "docs" },
+          autogenerate: { directory: "reference" },
         },
       ],
       customCss: ["./src/tailwind.css"],
+      components: {
+        Header: "./src/components/Header.astro",
+      },
       tableOfContents: false,
       pagination: false,
     }),
